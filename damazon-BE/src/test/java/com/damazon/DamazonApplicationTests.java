5d9a6ab3bc7a26c70512a7b1cd6361bdc9575110
Jaeyoung Ahn <quaki@naver.com>
2023-07-23 19:29:29 +0900

    init : 스프링 부트 기본 세팅

---
+++ damazon-BE/src/test/java/com/damazon/DamazonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damazon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DamazonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
